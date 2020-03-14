3d2a5133c0c379057cb351d09af11932e735e5ee
oscar <pcsultar27@gmail.com>
2020-03-14 13:46:43 -0600

    Se configuro logger

--- src/main/java/com/uatx/inventarios/InventariosApplication.java
+++ src/main/java/com/uatx/inventarios/InventariosApplication.java
@@ -1,12 +1,16 @@
 package com.uatx.inventarios;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventariosApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(InventariosApplication.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
+		LOGGER.debug("HOla desde el logger");
 		SpringApplication.run(InventariosApplication.class, args);
 	}
 
