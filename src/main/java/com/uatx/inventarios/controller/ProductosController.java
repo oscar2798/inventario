f5cf4ff9d9a6ae441e58a529e20471db169fe5cb
oscar <pcsultar27@gmail.com>
2020-03-19 13:55:43 -0600

    se actualizo solamente

--- src/main/java/com/uatx/inventarios/controller/ProductosController.java
+++ src/main/java/com/uatx/inventarios/controller/ProductosController.java
@@ -1,4 +1,25 @@
 package com.uatx.inventarios.controller;
 
+import com.uatx.inventarios.dto.ProductoDTO;
+import com.uatx.inventarios.services.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/productos")
 public class ProductosController {
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping("/guardar")
+    @ResponseBody
+    public Long guardarProducto(@RequestParam String nombre){
+        ProductoDTO productoDTO = new ProductoDTO();
+        productoDTO.setNombre(nombre);
+        return productoService.store(productoDTO);
+    }
 }
